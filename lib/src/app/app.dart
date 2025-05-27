82ce3763870d4e4a72e32c35ea3ed39b4a527e51
unisha21 <unishatamang02@gmail.com>
2025-05-27 14:54:41 +0545

    feat: Integrate KhaltiScope for payment processing in MainApp

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:homestay_app/src/common/route_manager.dart';
 import 'package:homestay_app/src/common/splash_screen.dart';
 import 'package:homestay_app/src/themes/theme.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -10,13 +11,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme(),
-      home: const SplashScreen(),
+    return KhaltiScope(
+      publicKey: 'test_public_key_599f6b5632ee4bfabc89b6d5dc55234d',
+      enabledDebugging: true,
       navigatorKey: navigatorKey,
-      onGenerateRoute: RouteGenerator.generateRoute,
-      initialRoute: Routes.splashRoute,
+      builder: (context, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: lightTheme(),
+          home: const SplashScreen(),
+          navigatorKey: navigatorKey,
+          onGenerateRoute: RouteGenerator.generateRoute,
+          initialRoute: Routes.splashRoute,
+          supportedLocales: const [Locale('en', 'US'), Locale('ne', 'NP')],
+          localizationsDelegates: const [KhaltiLocalizations.delegate],
+        );
+      },
     );
   }
 }
