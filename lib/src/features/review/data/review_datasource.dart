ef12905b17cdcd2da81ce0ac017445bb01610352
unisha21 <unishatamang02@gmail.com>
2025-05-30 23:09:59 +0545

    feat: Implement ReviewDataSource and ReviewModel for review management functionality

---
+++ lib/src/features/review/data/review_datasource.dart
@@ -0,0 +1,38 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:homestay_app/src/features/review/domain/review_model.dart';
+
+class ReviewDataSource{
+
+
+  final _reviewDb = FirebaseFirestore.instance.collection('reviews');
+
+  Future<String> addReview(ReviewModel reviewModel) async {
+    try {
+      await _reviewDb.add(reviewModel.toJson());
+      return 'Review Added Successfully';
+    } on FirebaseException catch (e) {
+      throw e.message.toString();
+    }
+  }
+
+  Stream<List<ReviewModel>> getReviewsStream(String menuId) {
+    try {
+      final data = _reviewDb.where('homeStayId', isEqualTo: menuId).snapshots();
+      final response = data.asyncMap((event) async {
+        final data = Future.wait(event.docs.map((e) async {
+          final json = e.data();
+          return ReviewModel.fromJson({
+            ...json,
+            'reviewId': e.id,
+          });
+        }).toList());
+        return data;
+      });
+      return response;
+    } on FirebaseException catch (error) {
+      throw '$error';
+    }
+  }
+
+}
